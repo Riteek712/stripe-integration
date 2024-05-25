a36a45d60c06e54caca2131a25064bea0a1a40ad
Riteek Rakesh <riteek7.12@gmail.com>
2024-05-25 09:41:52 +0530

    basic structure

---
+++ cmd/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	// "github.com/stripe/stripe-go"
+	// "github.com/stripe/stripe-go/paymentintent"
+	// "github.com/stripe/stripe-go/invoice"
+)
+
+func main() {
+	fmt.Println("Making Requests")
+
+}
