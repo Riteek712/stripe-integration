e0c44ae115515977b075d7dfd509649a01ac4095
Riteek Rakesh <riteek7.12@gmail.com>
2024-05-25 09:58:25 +0530

    stripe workplace setup

--- cmd/stripeIntegration/main.go
+++ cmd/stripeIntegration/main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"fmt"
-	// "github.com/stripe/stripe-go"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/stripe/stripe-go"
 	// "github.com/stripe/stripe-go/paymentintent"
 	// "github.com/stripe/stripe-go/invoice"
 )
 
 func main() {
+	godotenv.Load()
+	secretKey := os.Getenv("SecretKey")
+	stripe.Key = secretKey
 	fmt.Println("Making Requests")
+	// fmt.Println(secretKey)
 
 }
---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/Riteek712/stripe-integration
+
+go 1.22.0
+
+require github.com/stripe/stripe-go v70.15.0+incompatible
+
+require github.com/joho/godotenv v1.5.1 // indirect
